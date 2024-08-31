36011e2a1e617ebda92e65480918ba65807d0b31
Rahul Kumar Sah <rahulrks9805@gmail.com>
2024-08-31 14:01:34 +0530

    learning

---
+++ 3math2.js
@@ -0,0 +1,4 @@
+const math = require("./2math1");
+
+console.log(math);
+console.log(math(6,5));
---
+++ 5math4.js
@@ -0,0 +1,22 @@
+const math = require("./4math3");
+
+console.log(math);
+
+console.log(math.add);
+console.log(math.sub);
+
+console.log(math.add(2,9));
+console.log(math.sub(45,5));
+
+
+
+// ------------------------------------------------
+
+
+const {add,sub} = require("./4math3");
+
+console.log(add);
+console.log(sub);
+
+console.log(add(2,34));
+console.log(sub(42,10));
