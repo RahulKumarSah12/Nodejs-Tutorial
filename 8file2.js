3be59724adbf9c02865074734a2ba4963ec897a9
Rahul Kumar Sah <rahulrks9805@gmail.com>
2024-09-02 00:06:15 +0530

    Difference between Sync and ASync

---
+++ 8file2.js
@@ -0,0 +1,22 @@
+// Difference between Sync and ASync
+
+const fs = require("fs");
+
+// console.log("Rahul1");
+
+// const result = fs.readFileSync("./myfile/1test.txt", "utf-8");      // Sync -> Blocking
+// console.log(result);
+
+// console.log("Rohit1");
+
+
+// --------------------------------------------
+
+
+console.log("Rahul2");
+
+fs.readFile("./myfile/2test.txt", "utf-8", (err,res)=>{         // ASync -> Non-Blocking
+    console.log(res);
+});     
+
+console.log("Rohit2");
